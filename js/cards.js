881dc566d75c83e874ec71fbbe6a28877d58ed08
leonardo2322 <joys23services@gmail.com>
2023-02-10 10:22:24 -0500

    arreglando detalle del compra

--- js/cards.js
+++ js/cards.js
@@ -138,8 +138,9 @@ function whatsappSend(e) {
     let texto = mensaje + "  "+"Especificacion para los platos: " + inputEspesification.value + "   envia este mensaje te atenderemos enseguida"
     let what =`https://api.whatsapp.com/send?phone=+573502117928&text=${texto}`
     inputEspesification.value =  ""
-    Enviar.setAttribute('href',`${what}`)
     vaciarCarrito()
+
+    Enviar.setAttribute('href',`${what}`)
 
   }
 }
